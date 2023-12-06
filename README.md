c038677ee5e85f2578af952a4e74e62f14f0c331
Spencer Cox <spencox@gmail.com>
2023-12-06 10:42:59 -0600

    have core funcitonality of all required elements, next step is to try to get extra credit with delete functionality

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 
 
 cite: https://git.bootcampcontent.com/University-of-Texas-at-Austin/UTA-VIRT-FSF-PT-09-2023-U-LOLC/-/tree/main/11-Express/01-Activities/21-Ins_Modular-Routing/02_Modularized?ref_type=heads 
+
+Cite: https://nodejs.org/dist/latest-v6.x/docs/api/fs.html#fs_fs_readfile_file_options_callback
+
+CITE: https://git.bootcampcontent.com/University-of-Texas-at-Austin/UTA-VIRT-FSF-PT-09-2023-U-LOLC/-/tree/main/11-Express/01-Activities/22-Stu_Modular-Routing?ref_type=heads
--- routes/notes.js
+++ routes/notes.js
@@ -3,6 +3,7 @@ const path = require('path');
 
 // Helper functions for reading and writing to the JSON file
 const fs = require('fs');
+const uuid = require('../helpers/uuid');
 
 // Specify the path to the JSON file
 const filePath = path.join(__dirname, '..', '/db/db.json');
@@ -29,27 +30,60 @@ notes.get('/', (req, res) => {
     });
 });
   
-// This API route is a GET Route for retrieving all the tips
+// This API route is to post a note to database
 notes.post('/', (req, res) => {
     console.info(`${req.method} request received`);
-    // Read the content of the JSON file
   
     fs.readFile(filePath, 'utf8', (err, data) => {
         if (err) {
             console.error('Error reading the file:', err);
             return res.status(500).send('Internal Server Error');
         } 
-        try {
-            const parsedData = JSON.parse(data);
-            parsedData.push(req.body);
-            //writeToFile(file, parsedData);
-            fs.writeFile(filePath, JSON.stringify(parsedData), (err) => err ? console.error(err) : console.info(`\nData written to ${filePath}`));
-            res.status(201).send('File Created');
-        } catch (writeError) {
-            console.error('Error writing to db.JSON:', writeError);
-            res.status(500).send('Internal Server Error');
+        let parsedData = []; // if file is empty
+        if (data) {
+            try {
+                parsedData = JSON.parse(data);
+            } catch (parseError) {
+                console.error('Error parsing JSON:', parseError);
+                return res.status(500).send('Internal Server Error');
+            }
         }
+      
+        const newNote = {
+            id: uuid(),
+            title: req.body.title,
+            text: req.body.text
+        }
+        
+        parsedData.push(newNote);
+    
+        fs.writeFile(filePath, JSON.stringify(parsedData), (err) => err ? console.error(err) : console.info(`\nData written to ${filePath}`));
+
+        res.status(201).send('File Created');
     });
+    
+    // // This API route is to delete an note
+    // notes.delete('/', (req, res) => {
+    //     console.log("DELETE REQUEST MADE")
+
+    //     // Read the content of the JSON file
+    //     fs.readFile(filePath, 'utf8', (err, data) => {
+    //         if (err) {
+    //             console.error('Error reading the file:', err);
+    //             return res.status(500).send('Internal Server Error');
+    //         }
+    //         // Parse the JSON data
+    //         try {
+    //             const jsonData = JSON.parse(data);
+    //             //console.log('Data from db.json:', jsonData);
+    //             res.json(jsonData);
+    //         } catch (parseError) {
+    //             console.error('Error parsing JSON:', parseError);
+    //             res.status(500).send('Internal Server Error');
+    //         }
+    //     });
+    // });
+
 });
   
 
